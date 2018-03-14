25a0561ecf8b973e00b77844b15dcda5a2d75c95
Alexandr Novozhilov <alex.newlifer@gmail.com>
2018-03-14 22:43:41 +0300

    Move macro into separate module

--- fpu/fpu.sv
+++ fpu/fpu.sv
@@ -11,25 +11,11 @@
  * Creating date: 2018-02-12
  *
  */
+`include "macro.sv"
 
 
- /* Exponent size macro.
-  */
-`define EXP_SIZE(bintess) (bitness == 256? 19: bitness == 128? 15: bitness == 64? 11: bitness == 32? 8: 5)
-
-/* Mantissa size macro.
+/* Operation type.
  */
-`define MANT_SIZE(bitness) (bitness == 256? 236: bitness == 128? 112: bitness == 64? 52: bitness == 32? 23: 10)
-
-/* Macro with exponent's bias coefficient.
- */
-`define BIAS_COEFF(bitness) ((2 ** (`EXP_SIZE(bitness) - 1)) - 1)
-
-/* Max exponent value.
- */
-`define MAX_EXP_VALUE(bitness) (2 ** (`EXP_SIZE(bitness) - 1))
-
-
 typedef enum logic[3:0] {
                 add = 4'b0000  // Sum A and B
         ,       sub = 4'b0001  // Subtract A from B
@@ -37,6 +23,7 @@ typedef enum logic[3:0] {
         ,       div = 4'b0011  // Division A by B
                                // Other values are reserved
         } Operation_t;
+
 
 module fpu
         #(parameter bitness=32)
---
+++ fpu/macro.sv
@@ -0,0 +1,23 @@
+ /*
+ * Common FPU module macros.
+ *
+ * Author: Aleksandr Novozhilov
+ * Creating date: 2018-03-14
+ *
+ */
+
+ /* Exponent size macro.
+  */
+`define EXP_SIZE(bintess) (bitness == 256? 19: bitness == 128? 15: bitness == 64? 11: bitness == 32? 8: 5)
+
+/* Mantissa size macro.
+ */
+`define MANT_SIZE(bitness) (bitness == 256? 236: bitness == 128? 112: bitness == 64? 52: bitness == 32? 23: 10)
+
+/* Macro with exponent's bias coefficient.
+ */
+`define BIAS_COEFF(bitness) ((2 ** (`EXP_SIZE(bitness) - 1)) - 1)
+
+/* Max exponent value.
+ */
+`define MAX_EXP_VALUE(bitness) (2 ** (`EXP_SIZE(bitness) - 1))
